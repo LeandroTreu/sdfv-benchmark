3f371ca77416b23eaac25f5fcb075940188daa8f
LeandroTreu <leandro.treu@gmail.com>
2024-04-25 13:59:25 +0200

    directory restructure, tsconfig

--- profile_parser/src/plot_stats_comparison.ts
+++ profile_parser/src/plot_stats_comparison.ts
@@ -39,7 +39,7 @@ for (let i = 0; i < files_in_directory.length; ++i) {
 }
 
 const layout = {
-    title: "Statistics Comparison",
+    title: "Frametime Comparison",
     yaxis: {title: "Frametime (ms)"},
     shapes: [
         // 60 fps line
